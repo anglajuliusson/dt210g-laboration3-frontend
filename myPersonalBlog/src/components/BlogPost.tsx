e9c6064b73c1b57e3953fc6820c91b0f28aca18a
Ängla Juliusson <angla02@icloud.com>
2026-02-18 16:33:01 +0100

    Klickbara inlägg och stylling för dem

--- myPersonalBlog/src/components/BlogPost.tsx
+++ myPersonalBlog/src/components/BlogPost.tsx
@@ -1,12 +1,33 @@
 import type { BlogPostsInterface } from "../interfaces/BlogPostsInterface";
 
+const LinkStyle = {
+    textDecoration: "none",
+    color: "rgb(56, 56, 56)",
+    display: "flex",
+    gap: "3em"
+}
+
+const BlogPostStyle = {
+    margin: "2em auto 5em auto",
+    backgroundColor: "rgb(255, 243, 243)",
+    listStyleType: "none",
+    padding: "2em",
+    borderRadius: "10px",
+    marginBottom: "5em",
+    width: "fit-content"
+}
+
 function BlogPost({blogPostProp} : {blogPostProp: BlogPostsInterface}) {
     return (
-        <li>
-            <img src={blogPostProp.image}></img> {/* Visar bild för blogginlägg */}
-            <h3>{blogPostProp.title}</h3> {/* Visar titel för blogginlägg */}
-            <p>{blogPostProp.description}</p> {/* Visar texten för ett blogginlägg*/}
-            <p>Publicerat: {blogPostProp.date?.slice(0, 10)}</p> {/* Visar datumet för blogginlägget, utskrivet som yyyy-mm-dd */}
+        <li style={BlogPostStyle}>
+            <a style={LinkStyle} href={`/blog-posts/${blogPostProp.id}`}>
+                <img style={{ maxWidth: "400px", borderRadius: "5px"}} src={blogPostProp.image}></img> {/* Visar bild för blogginlägg */}
+                <div className="BlogPostText">
+                    <h2 style={{marginBottom: "1em", fontSize: "28px", color: "rgb(255, 111, 132)"}}>{blogPostProp.title}</h2> {/* Visar titel för blogginlägg */}
+                    <p style={{marginBottom: "1em", maxWidth: "500px"}}>{blogPostProp.description}</p> {/* Visar texten för ett blogginlägg*/}
+                    <p>Publicerat: {blogPostProp.date?.slice(0, 10)}</p> {/* Visar datumet för blogginlägget, utskrivet som yyyy-mm-dd */}
+                </div>
+            </a>
         </li>
     )
 }
