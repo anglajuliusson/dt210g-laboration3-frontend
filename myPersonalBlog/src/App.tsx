cac8ddfe9027e15999ae771afb838e5506f83eed
Ängla Juliusson <angla02@icloud.com>
2026-02-18 15:01:35 +0100

    Importera och skriv ut BlogPostList i App.tsx

--- myPersonalBlog/src/App.tsx
+++ myPersonalBlog/src/App.tsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header" // Importera header-komponent
 import Footer from "./components/Footer" // Importera footer-komponent
+import BlogPostsList from "./components/BlogPostList"
 
 function App() {
 
   return (
     <>
       <Header />
+      <BlogPostsList />
       <Footer />
     </>
   )
