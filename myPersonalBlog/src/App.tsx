16176144d9c868d24f729cadfb09726450afcefe
Ängla Juliusson <angla02@icloud.com>
2026-02-19 11:04:13 +0100

    Ta bort import och utskrift av komponenter i App.tsx

--- myPersonalBlog/src/App.tsx
+++ myPersonalBlog/src/App.tsx
@@ -1,14 +1,7 @@
-import Header from "./components/Header" // Importera header-komponent
-import Footer from "./components/Footer" // Importera footer-komponent
-import BlogPostsList from "./components/BlogPostList"
-
 function App() {
 
   return (
     <>
-      <Header />
-      <BlogPostsList />
-      <Footer />
     </>
   )
 }
