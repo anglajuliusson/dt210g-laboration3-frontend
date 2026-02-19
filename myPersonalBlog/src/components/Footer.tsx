5088d5a9e10f19ca528df56a80c05444679bf6b0
Ängla Juliusson <angla02@icloud.com>
2026-02-19 14:42:31 +0100

    Länk till logginsidan och styling för den i footer

--- myPersonalBlog/src/components/Footer.tsx
+++ myPersonalBlog/src/components/Footer.tsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 // Inline-styling för footer
 const footerStyling = {
     backgroundColor: "lightpink",
     color: "rgb(56, 56, 56)",
     padding: "2em",
+    display: "flex",
+    alignItems: "center"
 }
 
 const taskStyling = {
@@ -11,14 +15,22 @@ const taskStyling = {
     padding: "1em",
     fontSize: "15px"
 }
+const LinkStyle = {
+    color: "rgb(56, 56, 56)",
+}
 
 function Footer() {
     return (
         <footer>
             <div style={footerStyling}>
-                <h3 style={{color: "rgb(255, 111, 132)", marginBottom: "20px", fontSize: "18px"}}>Ängla Juliusson</h3>
-                <p style={{marginBottom: "10px", fontSize: "15px"}}>070-318 13 98</p>
-                <p style={{fontSize: "15px"}}>anju2402@student.miun.se</p>
+                <div>
+                    <h3 style={{color: "rgb(255, 111, 132)", marginBottom: "20px", fontSize: "18px"}}>Ängla Juliusson</h3>
+                    <p style={{marginBottom: "10px", fontSize: "15px"}}>070-318 13 98</p>
+                    <p style={{fontSize: "15px"}}>anju2402@student.miun.se</p>
+                </div>
+                <div style={{marginLeft: "auto"}}>
+                    <Link to="/login" style={LinkStyle}>Logga in</Link>
+                </div>
             </div>
             <div style={taskStyling}>
                 <p style={{textAlign: "center"}}>DT210G - Laboration 3 | VT26</p>
